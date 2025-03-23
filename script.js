dd51ae0d357e067795070ebaa8c69d7e24f8718f
Lakshya Raj <63673413+lraj22@users.noreply.github.com>
2025-03-23 09:34:23 -0700

    make everything work

--- script.js
+++ script.js
@@ -13,61 +13,112 @@ toAuth.addEventListener("click", function () {
 	sixDigits.focus();
 });
 
+toPassword.addEventListener("click", function () {
+	document.body.className = "state-password";
+	password.focus();
+});
+
+var emailDomainList = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "icloud.com"];
+toOTP.addEventListener("click", function () {
+	var givenUsername = username.value || "a user";
+	document.body.className = "state-otp";
+	otpDigits.focus();
+	email.textContent = givenUsername.slice(0)[0] + "******" + givenUsername.slice(-1)[0] + "@" + emailDomainList[givenUsername.split("").map(function (c) { return c.charCodeAt(0); }).reduce(function (acc, curr) { return acc + curr; }, 0) % emailDomainList.length];
+});
+
 toHSK.addEventListener("click", function () {
 	requestHSK();
 });
 
-submitSixDigits.addEventListener("click", function () {
-	var code = sixDigits.value;
-	if (code.length !== 6) return;
-	if (code.match(/^\d*7\d*2\d*$/) !== null) {
-		giveAccess();
-	}
+toPasskey.addEventListener("click", async function () {
+	await requestPasskey();
 });
 
-function requestPasskey () {
+submitSixDigits.addEventListener("click", function () {
+	var code = sixDigits.value;
+	if (code.length !== 6) {
+		iziToast.error({ message: "Code is not six digits" });
+		return;
+	}
+	if (code.match(/^\d*7\d*2\d*$/) !== null) {
+		giveAccess();
+	} else iziToast.error({ message: "Invalid code" } );
+});
+
+submitOtpDigits.addEventListener("click", function () {
+	var code = otpDigits.value;
+	if (code.length !== 6) {
+		iziToast.error({ message: "Code is not six digits" });
+		return;
+	}
+	if (code.match(/^\d*7\d*2\d*$/) !== null) {
+		giveAccess();
+	} else iziToast.error({ message: "Invalid code" } );
+});
+
+submitPassword.addEventListener("click", function () {
+	var pwd = password.value;
+	if (pwd.length < 8) {
+		iziToast.error({ message: "Password too short" });
+		return;
+	}
+	if (username.value && (pwd.indexOf(username.value) === -1)) {
+		iziToast.error({ message: "Incorrect password" });
+		return;
+	}
+	giveAccess();
+});
+
+iziToast.settings({
+	position: "topRight",
+	timeout: 3000,
+});
+
+async function requestPasskey () {
 	if (window.PublicKeyCredential &&
 		PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable &&
 		PublicKeyCredential.isConditionalMediationAvailable) {
 		Promise.all([
 			PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
 			PublicKeyCredential.isConditionalMediationAvailable(),
-		]).then(results => {
+		]).then(async function (results)  {
 			if (results.every(r => r === true)) {
-				actuallyRequestPasskey();
+				await actuallyRequestPasskey();
 			}
 		});
 	}
 }
 
-function actuallyRequestPasskey () {
-	// Example `PublicKeyCredentialCreationOptions` contents
-	navigator.credentials.create({
-		challenge: generateChallenge(16),
-		rp: {
-			name: "Security Demo",
-			id: "localhost",
-		},
-		user: {
-			id: "*****",
-			name: "john78",
-			displayName: "John",
-		},
-		pubKeyCredParams: [{
-			alg: -7, type: "public-key"
-		},{
-			alg: -257, type: "public-key"
-		}],
-		excludeCredentials: [{
-			id: "*****",
-			type: 'public-key',
-			transports: ['internal'],
-		}],
-		authenticatorSelection: {
-			authenticatorAttachment: "platform",
-			requireResidentKey: true,
+async function actuallyRequestPasskey () {
+	try{
+	const response = await navigator.credentials.create({
+		publicKey: {
+			challenge: generateChallenge(16),
+			rp: {
+				name: "Security Demo",
+				id: location.hostname,
+			},
+			user: {
+				id: generateChallenge(16),
+				name: username.value || "a user",
+				displayName: username.value || "a user",
+			},
+			pubKeyCredParams: [{
+				alg: -7, type: "public-key"
+			}, {
+				alg: -257, type: "public-key"
+			}],
+			authenticatorSelection: {
+				authenticatorAttachment: "platform",
+				requireResidentKey: true,
+			},
+			
 		}
 	});
+	giveAccess();
+} catch (e) {
+	iziToast.error({ message: "Passkey auth failed." });
+}
 }
 
 function requestHSK () {
@@ -98,5 +149,5 @@ function generateChallenge (l) {
 }
 
 function giveAccess () {
-	alert("YOU SUCCESSFULLY LOGGED IN!");
+	iziToast.success({ message: "Login successful!" });
 }
